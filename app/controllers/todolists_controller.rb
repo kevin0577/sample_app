cc85dbf5dd197da5d7597ad941fa6ea222edd04b
梶ヶ谷篤哉 <kajigayaatsuya@kajigayaatsuyanoMacBook-Air.local>
2020-03-05 22:53:31 +0900

    Start 3-7

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -26,7 +26,17 @@ class TodolistsController < ApplicationController
 
         redirect_to todolist_path(list.id) # 詳細画面へリダイレクト
         end
-
+　   def edit
+        @list = List.find(params[:id])
+    end
+def update
+    end
+def update
+        list = List.find(params[:id])
+        list.update(list_params)
+        redirect_to todolist_path(list.id)
+    end
+    
     private
 
     def list_params
