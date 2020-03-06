1d1cf191dc77b745b02ca32758f048ef38a4792c
梶ヶ谷篤哉 <kajigayaatsuya@kajigayaatsuyanoMacBook-Air.local>
2020-03-06 18:59:22 +0900

    finish 3-8

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'a3b067cb465e9ef31fd1fc1f8c4d85335e2bb2fc70e899ba7acc00af399d4d32498acaaa1e4cf68d1363e2270c27ea21059a8a78414ca16cd1280210363eb888'
